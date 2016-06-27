bc35692b35e4c299ff3755f716f533be5fb7c258
ric <ric@192.168.1.40>
2016-06-27 16:29:12 -0300

    Encapsulamento correto

--- src/vfm/model/Submarino.java
+++ src/vfm/model/Submarino.java
@@ -8,8 +8,42 @@ package vfm.model;
  * @author ric
  */
 public class Submarino {
-    public int x;
-    public int y;
-    public int z;
-    public String direcao;
+    private int x;
+    private int y;
+    private int z;
+    private String direcao;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public String getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(String direcao) {
+        this.direcao = direcao;
+    }
+    
+    
 }
